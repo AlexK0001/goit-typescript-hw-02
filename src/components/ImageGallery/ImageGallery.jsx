baaed5f2615e6959b80c2a0cbff227992b625fe4
Alexandr <148452094+AlexK0001@users.noreply.github.com>
2024-10-04 14:38:42 +0300

    Initial commit

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,14 @@
+import ImageCard from '../ImageCard/ImageCard';
+import css from './ImageGallery.module.css'
+
+export default function ImageGallery({ images, handleImageClick, openModal }) {
+    return (
+        <ul className={css.images}>
+            {images.map(image => (
+                <li key={image.id}>
+                    <ImageCard image={image} onImageClick={handleImageClick} openModal={openModal}/>
+                </li>
+            ))}
+        </ul>
+    );
+}
